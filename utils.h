c486b5193093a6dafc62eb6ce787b3e3b2a583ba
shadman chowdhury <81127900+shad902@users.noreply.github.com>
2022-03-12 18:05:28 -0500

    Add files via upload

---
+++ utils.h
@@ -0,0 +1,14 @@
+
+#ifndef _utils_h_guard
+#define _utils_h_guard
+
+void delay (int x);
+
+/************************************************************/
+void store(int value, int addr);
+int load(int addr);
+/*******************************END**************************/
+void delay_ms(int x/1000);
+void delay_us(int x/1000000);
+
+#endif
